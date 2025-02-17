2a7797f4babc621f48a4ab8c6b70d9717e875247
Tee Teerasak <teerasak.n@existing.co>
2025-02-17 15:08:45 +0700

    [🧪] update config

--- src/infrastructure/config/config.ts
+++ src/infrastructure/config/config.ts
@@ -11,7 +11,7 @@ export default () => ({
     type: 'postgres',
     host: process.env.DB_HOST || 'localhost',
     port: parseInt(process.env.DB_PORT, 10) || 5432,
-    username: process.env.DB_USERNAME || 'postgres',
+    username: process.env.DB_USERNAME || 'user',
     password: process.env.DB_PASSWORD || 'password',
     database: process.env.DB_NAME || 'dbname',
   },
