c9ed4319e51c599427cd012618131e3269b00c9b
Tee Teerasak <teerasak.n@existing.co>
2025-02-20 10:32:03 +0700

    [➕] update grpc

--- src/interfaces/grpc/grpc.module.ts
+++ src/interfaces/grpc/grpc.module.ts
@@ -2,10 +2,9 @@ import { Module } from '@nestjs/common';
 import { Transport, MicroserviceOptions } from '@nestjs/microservices';
 import { join } from 'path';
 import { UserController } from './user.controller';
-import { UserService } from './user.service';
 
 @Module({
   controllers: [UserController],
-  providers: [UserService],
+  providers: [],
 })
 export class GrpcModule {}
--- src/interfaces/grpc/user.controller.ts
+++ src/interfaces/grpc/user.controller.ts
@@ -1,19 +1,18 @@
 import { Controller } from '@nestjs/common';
 import { GrpcMethod } from '@nestjs/microservices';
-import { UserService } from './user.service';
 import { CreateUserRequest, GetUserRequest, UserResponse } from './user.interface';
 
 @Controller()
 export class UserController {
-  constructor(private readonly userService: UserService) {}
+  constructor() {}
 
   @GrpcMethod('UserService', 'GetUser')
   async getUser(request: GetUserRequest): Promise<UserResponse> {
-    return this.userService.getUser(request);
+    return {id: request.id, name: "", email: ""};
   }
 
   @GrpcMethod('UserService', 'CreateUser')
   async createUser(request: CreateUserRequest): Promise<UserResponse> {
-    return this.userService.createUser(request);
+    return {id: "1", name: request.name, email: request.email};
   }
 }
